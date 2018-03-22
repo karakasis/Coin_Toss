9d358640df1585461c29ce4f4d6b680e601054d9
karakasis <karakasisx@gmail.com>
2018-03-22 17:45:21 +0200

    [dev-options fully added] Added scroll view for strikes add Added alternate landscape view Almost final 1 Bug : after (13,13,18 strikes) index strikes arraylist will produce null saying size:13 index = -1.

--- app/src/main/java/com/philip/coin_toss/FlipListener.java
+++ app/src/main/java/com/philip/coin_toss/FlipListener.java
@@ -20,47 +20,54 @@ public class FlipListener implements ValueAnimator.AnimatorUpdateListener {
   public void onAnimationUpdate(final ValueAnimator animation) {
     float value = animation.getAnimatedFraction();
     final float animValue = (float) animation.getAnimatedValue();
-    float scaleValue;
-    if (MainActivity.alternateFlipEffect) {
-      value = value * 100f;
-      //functions calculated with a linear interpolation with 8 data points
-      //https://tools.timodenk.com/linear-interpolation
-      if (value <= 2) {
-        scaleValue = -12.5f * value + 100f;
-      } else if (value > 2 && value <= 15) {
-        scaleValue = 1.9231f * value + 7.1154f;
-      } else if (value > 15 && value <= 35) {
-        scaleValue = 2f * value + 70f;
-      } else if (value > 35 && value <= 50) {
-        scaleValue = 1.3333f * value + 93.3333f;
-      } else if (value > 50 && value <= 70) {
-        scaleValue = -1.2f * value + 220f;
-      } else if (value > 70 && value <= 80) {
-        scaleValue = -1.1f * value + 213f;
-      } else if (value > 80 && value <= 90) {
-        scaleValue = -2f * value + 285f;
-      } else {
-        scaleValue = -0.5f * value + 150f;
-      }
+    float scaleValue = 0f;
+    if (!MainActivity.disableScaleFlipEffect) {
+      if (MainActivity.alternateFlipEffect) {
+        value = value * 100f;
+        //functions calculated with a linear interpolation with 8 data points
+        //https://tools.timodenk.com/linear-interpolation
+        if (value <= 2) {
+          scaleValue = -12.5f * value + 100f;
+        } else if (value > 2 && value <= 15) {
+          scaleValue = 1.9231f * value + 7.1154f;
+        } else if (value > 15 && value <= 35) {
+          scaleValue = 2f * value + 70f;
+        } else if (value > 35 && value <= 50) {
+          scaleValue = 1.3333f * value + 93.3333f;
+        } else if (value > 50 && value <= 70) {
+          scaleValue = -1.2f * value + 220f;
+        } else if (value > 70 && value <= 80) {
+          scaleValue = -1.1f * value + 213f;
+        } else if (value > 80 && value <= 90) {
+          scaleValue = -2f * value + 285f;
+        } else {
+          scaleValue = -0.5f * value + 150f;
+        }
 
-      scaleValue = 0.35f * scaleValue / 100f;
-    } else {
-      scaleValue = 0.6f - (1.5f * (value - 0.5f) * (value - 0.5f));
-      System.out.println(scaleValue + " " + value);
-      if (scaleValue <= 0.35 && value > 0.8) {
-        scaleValue = 0.35f;
+        scaleValue = 0.35f * scaleValue / 100f;
+      } else {
+        scaleValue = 0.6f - (1.5f * (value - 0.5f) * (value - 0.5f));
+        System.out.println(scaleValue + " " + value);
+        if (scaleValue <= 0.35 && value > 0.8) {
+          scaleValue = 0.35f;
+        }
       }
     }
 
     if (flipBounds(animValue)) {
       this.frontView.setRotationY(180 * animValue);
-      this.frontView.setScaleX(scaleValue);
-      this.frontView.setScaleY(scaleValue);
+      if(!MainActivity.disableScaleFlipEffect){
+
+        this.frontView.setScaleX(scaleValue);
+        this.frontView.setScaleY(scaleValue);
+      }
       setStateFlipped(false); //front view visible
     } else {
       this.backView.setRotationY(-180 * (1f - animValue));
-      this.backView.setScaleX(scaleValue);
-      this.backView.setScaleY(scaleValue);
+      if(!MainActivity.disableScaleFlipEffect){
+        this.backView.setScaleX(scaleValue);
+        this.backView.setScaleY(scaleValue);
+      }
       setStateFlipped(true); //back view visible
     }
   }
--- app/src/main/java/com/philip/coin_toss/MainActivity.java
+++ app/src/main/java/com/philip/coin_toss/MainActivity.java
@@ -2,6 +2,7 @@ package com.philip.coin_toss;
 
 import android.animation.TimeInterpolator;
 import android.animation.ValueAnimator;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,8 +14,10 @@ import android.view.animation.LinearInterpolator;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,9 +29,28 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
   //dev
-  private boolean devActive = true;
-  private boolean swapFlipCount = false;
+  private boolean firstExecution = true;
+  private boolean devActive = false;
+  private boolean cheatActive = false;
+  private boolean cheatResult;
+  private boolean plusFlips = false;
   public static boolean alternateFlipEffect = false;
+  public static boolean disableScaleFlipEffect = false;
+
+  private static final String DEV_KEY = "DEV";
+  private static final String CHEAT_KEY = "CHEAT";
+  private static final String PLUS_FLIP_KEY = "PLUS_FLIP";
+  private static final String ALT_FLIP_KEY = "ALT_FLIP";
+  private static final String DIS_SCALE_FLIP_KEY = "DIS_SCALE_FLIP";
+  private static final String CHEAT_RESULT_KEY = "CHEAT_RESULT";
+
+
+  private CheckBox devBox;
+  private CheckBox cheatBox;
+  private CheckBox plusFlipBox;
+  private CheckBox altFlipBox;
+  private CheckBox disScaleFlipBox;
+
   //dev
   private int swapFlips = 1;
 
@@ -36,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
   private EnumChoice result = EnumChoice.HEADS; // wont mess with the app results
   private boolean newHighscore = false;
   private Random rand = new Random();
+
 
   private enum EnumChoice {
     HEADS,
@@ -47,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
   private static final String STRIKES_KEY = "STRIKES";
   private static final String WAS_HEADS_KEY = "WAS_HEADS";
   private static final String IS_HEADS_KEY = "IS_HEADS";
+
   private int score = 0;
   private int highScore = 0;
   private ArrayList<String> strikes = new ArrayList<>();
@@ -63,27 +87,148 @@ public class MainActivity extends AppCompatActivity {
   private ValueAnimator scaleAnimator;
 
 
+  public void devMode(View view) {
+    if (devBox.isChecked()) {
+      devActive = true;
+      cheatBox.setVisibility(View.VISIBLE);
+      plusFlipBox.setVisibility(View.VISIBLE);
+      altFlipBox.setVisibility(View.VISIBLE);
+      disScaleFlipBox.setVisibility(View.VISIBLE);
+      if (cheatActive) {
+        cheatMode(null);
+      }
+      if (plusFlips) {
+        plusFlipMode(null);
+      }
+      if (alternateFlipEffect) {
+        altFlipMode(null);
+      }
+      if (disableScaleFlipEffect) {
+        disScaleFlipMode(null);
+      }
+    } else {
+      devActive = false;
+      cheatBox.setChecked(false);
+      cheatMode(null);
+      cheatBox.setVisibility(View.GONE);
+
+      plusFlipBox.setChecked(false);
+      plusFlipMode(null);
+      plusFlipBox.setVisibility(View.GONE);
+
+      altFlipBox.setChecked(false);
+      altFlipMode(null);
+      altFlipBox.setVisibility(View.GONE);
+
+      disScaleFlipBox.setChecked(false);
+      disScaleFlipMode(null);
+      disScaleFlipBox.setVisibility(View.GONE);
+    }
+  }
+
+  public void cheatMode(View view) {
+    cheatActive = cheatBox.isChecked();
+    if (cheatActive && view != null) {
+
+      Toast.makeText(this, "Correct choice"
+          + " will now be displayed with a RED border", Toast.LENGTH_LONG).show();
+    } else if (!cheatActive && view != null) {
+
+      Toast.makeText(this, "Cheat mode"
+          + " disabled. Borders are default color", Toast.LENGTH_LONG).show();
+    }
+    if (cheatActive) {
+      showCheatResult();
+    } else {
+      tailsButton.setBackgroundResource(R.drawable.buttonshape);
+      headsButton.setBackgroundResource(R.drawable.buttonshape);
+    }
+  }
+
+  private void showCheatResult() {
+    if (cheatActive) {
+      if (cheatResult == true) // HEADS
+      {
+        headsButton.setBackgroundResource(R.drawable.buttonshape_cheat);
+        tailsButton.setBackgroundResource(R.drawable.buttonshape);
+      } else {
+        tailsButton.setBackgroundResource(R.drawable.buttonshape_cheat);
+        headsButton.setBackgroundResource(R.drawable.buttonshape);
+      }
+    } else {
+      tailsButton.setBackgroundResource(R.drawable.buttonshape);
+      headsButton.setBackgroundResource(R.drawable.buttonshape);
+    }
+
+  }
+
+  public void plusFlipMode(View view) {
+    plusFlips = plusFlipBox.isChecked();
+    if (plusFlips && view != null) {
+      swapFlips = 3;
+      Toast.makeText(this, "Swapping sides will"
+          + " now be animated with 3 flips", Toast.LENGTH_LONG).show();
+    } else if (!plusFlips && view != null) {
+      swapFlips = 1;
+      Toast.makeText(this, "Swapping sides will"
+          + " now be animated with 1 flip", Toast.LENGTH_LONG).show();
+    }
+    if (plusFlips && view == null) {
+      swapFlips = 3;
+    } else if (!plusFlips && view == null) {
+      swapFlips = 1;
+    }
+  }
+
+  public void altFlipMode(View view) {
+    MainActivity.alternateFlipEffect = altFlipBox.isChecked();
+    if (MainActivity.alternateFlipEffect && view != null) {
+      Toast.makeText(this, "The coin will now"
+          + " flip with modified animation (experimental)", Toast.LENGTH_LONG).show();
+    } else if (!MainActivity.alternateFlipEffect && view != null) {
+      Toast.makeText(this, "The coin will now"
+          + " flip with default animation", Toast.LENGTH_LONG).show();
+    }
+  }
+
+  public void disScaleFlipMode(View view) {
+    MainActivity.disableScaleFlipEffect = disScaleFlipBox.isChecked();
+    if (MainActivity.disableScaleFlipEffect && view != null) {
+      Toast.makeText(this, "The coin will now"
+          + " flip without scaling animation (also ++ flips)", Toast.LENGTH_LONG).show();
+    } else if (!MainActivity.disableScaleFlipEffect && view != null) {
+      Toast.makeText(this, "The coin will now"
+          + " flip with preset scale animation", Toast.LENGTH_LONG).show();
+    }
+  }
+
+  private void updateCheckboxState() {
+    devBox.setChecked(devActive);
+    cheatBox.setChecked(cheatActive);
+    plusFlipBox.setChecked(plusFlips);
+    altFlipBox.setChecked(alternateFlipEffect);
+    disScaleFlipBox.setChecked(disableScaleFlipEffect);
+  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    //dev
-    if(devActive){
-      result = result(rand.nextBoolean());
-    }
-
-    if(swapFlipCount){
-      swapFlips = 3;
-    }else{
-      swapFlips = 1;
-    }
-    //dev
-
     headsView = (ImageView) findViewById(R.id.headsViewXML);
     tailsView = (ImageView) findViewById(R.id.tailsViewXML);
     headsButton = (Button) findViewById(R.id.heads);
     tailsButton = (Button) findViewById(R.id.tails);
+    //dev
+    if (firstExecution) {
+      cheatResult = rand.nextBoolean();
+      firstExecution = false;
+    }
+    devBox = (CheckBox) findViewById(R.id.devBox);
+    cheatBox = (CheckBox) findViewById(R.id.cheatBox);
+    plusFlipBox = (CheckBox) findViewById(R.id.plusFlipBox);
+    altFlipBox = (CheckBox) findViewById(R.id.altFlipBox);
+    disScaleFlipBox = (CheckBox) findViewById(R.id.disScaleFlipBox);
 
     TranslateAnimation r2l = new TranslateAnimation(1500.0f, 0.0f, 0.0f,
         0.0f); // new TranslateAnimation(xFrom,xTo, yFrom,yTo)
@@ -112,6 +257,15 @@ public class MainActivity extends AppCompatActivity {
       strikes = new ArrayList<>(savedInstanceState.getStringArrayList(STRIKES_KEY));
       isDisplayingHeads = savedInstanceState.getBoolean(IS_HEADS_KEY);
       wasDisplayingHeads = savedInstanceState.getBoolean(WAS_HEADS_KEY);
+      //dev
+      firstExecution = savedInstanceState.getBoolean("first");
+      devActive = savedInstanceState.getBoolean(DEV_KEY);
+      cheatActive = savedInstanceState.getBoolean(CHEAT_KEY);
+      plusFlips = savedInstanceState.getBoolean(PLUS_FLIP_KEY);
+      alternateFlipEffect = savedInstanceState.getBoolean(ALT_FLIP_KEY);
+      disableScaleFlipEffect = savedInstanceState.getBoolean(DIS_SCALE_FLIP_KEY);
+      cheatResult = savedInstanceState.getBoolean(CHEAT_RESULT_KEY);
+      updateCheckboxState();
 
       if (isDisplayingHeads) {
         result = EnumChoice.HEADS;
@@ -125,6 +279,11 @@ public class MainActivity extends AppCompatActivity {
 
       updateScreen();
     }
+
+    //dev
+    devMode(devBox);
+
+    //dev
 
     scaleAnimator = ValueAnimator.ofFloat(0f, 1f);
     //mFlipAnimator.addListener(new FlipListenerEnd(this));
@@ -141,12 +300,22 @@ public class MainActivity extends AppCompatActivity {
     bundle.putStringArrayList(STRIKES_KEY, strikes);
     bundle.putBoolean(WAS_HEADS_KEY, wasDisplayingHeads);
     bundle.putBoolean(IS_HEADS_KEY, isDisplayingHeads);
+    //dev
+    bundle.putBoolean("first", firstExecution);
+    bundle.putBoolean(DEV_KEY, devActive);
+    bundle.putBoolean(CHEAT_KEY, cheatActive);
+    bundle.putBoolean(PLUS_FLIP_KEY, plusFlips);
+    bundle.putBoolean(ALT_FLIP_KEY, alternateFlipEffect);
+    bundle.putBoolean(DIS_SCALE_FLIP_KEY, disableScaleFlipEffect);
+    bundle.putBoolean(CHEAT_RESULT_KEY, cheatResult);
   }
 
   public void update(View view) {
 
-    if(!devActive){ //dev option
+    if (!cheatActive) { //dev option
       result = result(rand.nextBoolean());
+    } else {
+      result = result(cheatResult);
     }
 
     setupAnimation(randomFlips());
@@ -186,21 +355,29 @@ public class MainActivity extends AppCompatActivity {
     if (result == EnumChoice.HEADS) {
       if (wasDisplayingHeads) {
         // result = HEADS , previous result = HEADS , saved result = HEADS
-        //return 2 * (rand.nextInt(1) + 1);
+        if(MainActivity.disableScaleFlipEffect)
+        return 2 * (rand.nextInt(3) + 1);
+        else
         return 2;
       } else {
         // result = HEADS , previous result = TAILS , saved result = HEADS
-        //return 2 * (rand.nextInt(1) + 1) + 1;
+        if(MainActivity.disableScaleFlipEffect)
+        return 2 * (rand.nextInt(3) + 1) + 1;
+        else
         return swapFlips;
       }
     } else if (result == EnumChoice.TAILS) {
       if (wasDisplayingHeads) {
         // result = TAILS , previous result = HEADS , saved result = TAILS
-        //return 2 * (rand.nextInt(1) + 1) + 1;
+        if(MainActivity.disableScaleFlipEffect)
+        return 2 * (rand.nextInt(3) + 1) + 1;
+        else
         return swapFlips;
       } else {
         // result = TAILS , previous result = TAILS , saved result = TAILS
-        //return 2 * (rand.nextInt(1) + 1);
+        if(MainActivity.disableScaleFlipEffect)
+        return 2 * (rand.nextInt(3) + 1);
+        else
         return 2;
       }
     }
@@ -208,6 +385,9 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void updateScreen() {
+    //dev - changes result with every phone flip, but doesn't matter
+    cheatResult = rand.nextBoolean();
+    showCheatResult();
 
     TextView scoreTV = (TextView) findViewById(R.id.score);
     scoreTV.setText(String.valueOf(score));
@@ -215,17 +395,52 @@ public class MainActivity extends AppCompatActivity {
     highScoreTV.setText(String.valueOf(highScore));
     //
     LinearLayout linearLayout = (LinearLayout) findViewById(R.id.strikesLayout);
+
     linearLayout.removeAllViewsInLayout();
-    for (String ch : strikes) {
-      TextView textView = new TextView(this);
-      if (ch.equals("H")) {
-        textView.setText(R.string.strikesHeads);
-        textView.setTextAppearance(this, R.style.StrikeHApp);
+    if (strikes.size() == 1) {
+      //play animation
+      ImageView image = new ImageView(this);
+      image.setScaleType(ScaleType.CENTER);
+      image.setAdjustViewBounds(true);
+      if (strikes.get(0).equals("H")) {
+        image.setImageDrawable(getResources().getDrawable(R.drawable.heads));
       } else {
-        textView.setText(R.string.strikesTails);
-        textView.setTextAppearance(this, R.style.StrikeTApp);
+        image.setImageDrawable(getResources().getDrawable(R.drawable.tails));
       }
-      linearLayout.addView(textView);
+      linearLayout.addView(image);
+
+      TranslateAnimation r2l = new TranslateAnimation(1500.0f, 0.0f, 0.0f,
+          0.0f);
+      r2l.setDuration(650);
+      r2l.setFillAfter(true);
+      image.startAnimation(r2l);
+    } else {
+      for (int i = 0; i <= strikes.size() - 2; i++) {
+        ImageView image = new ImageView(this);
+        image.setScaleType(ScaleType.CENTER);
+        image.setAdjustViewBounds(true);
+        if (strikes.get(i).equals("H")) {
+          image.setImageDrawable(getResources().getDrawable(R.drawable.heads));
+        } else {
+          image.setImageDrawable(getResources().getDrawable(R.drawable.tails));
+        }
+        linearLayout.addView(image);
+      }
+      ImageView image = new ImageView(this);
+      image.setScaleType(ScaleType.CENTER);
+      image.setAdjustViewBounds(true);
+      if (strikes.get(strikes.size() - 1).equals("H")) {
+        image.setImageDrawable(getResources().getDrawable(R.drawable.heads));
+      } else {
+        image.setImageDrawable(getResources().getDrawable(R.drawable.tails));
+      }
+      linearLayout.addView(image);
+      //play animation
+      TranslateAnimation r2l = new TranslateAnimation(1500.0f, 0.0f, 0.0f,
+          0.0f);
+      r2l.setDuration(650);
+      r2l.setFillAfter(true);
+      image.startAnimation(r2l);
     }
     Log.v("MainActivity", "Update screen was called");
   }
